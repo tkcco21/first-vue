1ebc784d07cd6a29d5a35c9a9c83d4d1029ec639
tkcco <asd33tyu@gmail.com>
2019-02-14 09:55:54 +0900

    エラー出てる

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
     <h1 class="hello-title">
-      Hello Vue
+      {{ msg }}
     </h1>
+    <!-- <button-counter />
+    <button-counter />
+    <button-counter />
+    <button-counter /> -->
+    <blog-post />
   </div>
 </template>
 
-<style scoped lang="scss">
+<script type="text/javascript">
+import Vue from 'vue';
 
+export default {
+  name: 'HelloVue',
+  props: {
+    title: String
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App!!'
+    }
+  }
+}
+// button-counter と呼ばれる新しいコンポーネントを定義します
+// Vue.component('button-counter', {
+//   data: function() {
+//     return {
+//       count: 0
+//     }
+//   },
+//   template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+// })
+Vue.component('blog-post', {
+  props: ['title'],
+  template: '<h3>{{ title }}</h3>'
+})
+</script>
+
+<style scoped lang="scss">
 .hello {
   &-title {
     color: #00f;
