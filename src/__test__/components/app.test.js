14c00ce3736c4cd6f8ac02161314d23c333b9222
tkcco <asd33tyu@gmail.com>
2019-02-21 21:28:00 +0900

    edit test

--- src/__test__/components/app.test.js
+++ src/__test__/components/app.test.js
@@ -1,9 +1,9 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import App from '@/components/App';
 
 describe('Component', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(App)
+    const wrapper = shallowMount(App)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
