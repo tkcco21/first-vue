8717fbe48fc86da2dc77b1872a299bcb884c2826
tkcco <asd33tyu@gmail.com>
2020-03-28 19:00:50 +0900

    #78 add json server for admin route

--- mock/server.js
+++ mock/server.js
@@ -6,6 +6,8 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
+const TOKEN = 'development_user';
+
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 // To handle POST, PUT and PATCH you need to use a body-parser
@@ -15,6 +17,29 @@ server.use(jsonServer.bodyParser);
 server.get('/books/:id', (req, res) => {
   const book = router.db.__wrapped__.bookDetail.find(item => req.params.id == item.id);
   res.jsonp(book);
+});
+
+server.post('/books', (req, res) => {
+  if (req.headers.authorization.split(' ')[1] === TOKEN) {
+    return res.jsonp(req.body);
+  }
+  return res.sendStatus(401);
+});
+
+server.patch('/books/:id', (req, res) => {
+  if (req.headers.authorization.split(' ')[1] === TOKEN) {
+    return res.jsonp(req.body);
+  }
+  return res.sendStatus(401);
+});
+
+server.post('/admin/signin', (req, res) => {
+  const user = { name: 'admin', password: 'test123' };
+  const { username, password } = req.body;
+  if (username === user.name && password === user.password) {
+    return res.jsonp({ token: TOKEN });
+  }
+  return res.sendStatus(401);
 });
 
 server.use(router);
